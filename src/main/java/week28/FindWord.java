3693546accf04db0f409ffb4e7048885c10816fa
Kwon Tae-Yun <taeyun1118@naver.com>
2025-11-02 14:23:22 +0900

    25.11.02 문자 찾기 구현완료

---
+++ src/main/java/week28/FindWord.java
@@ -0,0 +1,21 @@
+package week28;
+
+import java.util.Scanner;
+
+public class FindWord {
+    public static void main(String[] args){
+        int answer = 0;
+        Scanner in= new Scanner(System.in);
+        String input1 = in.nextLine();          // 문자열 전체 읽기
+        char input2 = in.nextLine().charAt(0);
+        input1 = input1.toLowerCase();
+        input2 = Character.toLowerCase(input2);
+        for (char c : input1.toCharArray()) {
+            if(c == input2){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
